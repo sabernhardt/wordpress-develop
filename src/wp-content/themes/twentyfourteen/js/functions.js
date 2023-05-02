841d481b582f7860362b09351ab414cd86578ca2
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2023-05-02 16:06:02 -0500

    Add period in Twenty Fourteen script

--- src/wp-content/themes/twentyfourteen/js/functions.js
+++ src/wp-content/themes/twentyfourteen/js/functions.js
@@ -2,7 +2,7 @@
  * Theme functions file.
  *
  * Contains handlers for navigation, accessibility, header sizing,
- * footer widgets and Featured Content slider
+ * footer widgets and Featured Content slider.
  *
  */
 ( function( $ ) {
