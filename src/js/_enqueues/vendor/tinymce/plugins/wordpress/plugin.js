5a9feced0da0601d284795ce271db7bdcdeeeb4f
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2025-02-22 17:10:43 -0600

    Add class and related CSS, increase width and force height

--- src/js/_enqueues/vendor/tinymce/plugins/wordpress/plugin.js
+++ src/js/_enqueues/vendor/tinymce/plugins/wordpress/plugin.js
@@ -143,9 +143,9 @@ tinymce.PluginManager.add( 'wordpress', function( editor ) {
 						'src="' + tinymce.Env.transparentSrc + '" ' +
 						'data-wp-preserve="' + encodeURIComponent( match ) + '" ' +
 						'data-mce-resize="false" ' +
-						'data-mce-placeholder="1" '+
-						'class="mce-object" ' +
-						'width="20" height="20" '+
+						'data-mce-placeholder="1" ' +
+						'class="mce-object mce-object-' + tag + '" ' +
+						'width="70" height="20" style="height: 20px;" ' +
 						'alt="&lt;' + tag + '&gt;" ' +
 					'/>';
 				} );
