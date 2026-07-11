c3c4f2be4e6f5ab00bae31c21b40b73ca9bfabfb
Stephen A. Bernhardt <sabernhardt@yahoo.com>
2026-07-11 13:32:56 -0500

    Add rule in `postcss.config.js`, adjusting `:lang()` and `::first-letter`

--- src/wp-content/themes/twentynineteen/postcss.config.js
+++ src/wp-content/themes/twentynineteen/postcss.config.js
@@ -1,11 +1,29 @@
 var postcssFocusWithin = require('postcss-focus-within');
 var autoprefixer = require('autoprefixer');
 
+var oneSelectorPerLine = function () {
+    return {
+        postcssPlugin: 'one-selector-per-line',
+        Rule: function (rule) {
+            if (rule.selector.indexOf(',') !== -1) {
+                var before = rule.raws.before || '';
+                var indent = before.substring(before.lastIndexOf('\n') + 1);
+                rule.selector = rule.selector.replace(/, :lang/g, ',:lang').split(/,\s+/).join(',\n' + indent);
+            }
+            if (rule.selector.indexOf(':not(:focus)::first-letter:') !== -1) {
+                rule.selector = rule.selector.replace(':not(:focus)::first-letter', '') + ':not(:focus)::first-letter';
+            }
+        }
+    };
+};
+oneSelectorPerLine.postcss = true;
+
 module.exports = {
     plugins: [
         postcssFocusWithin({
             disablePolyfillReadyClass: true
         }),
+		oneSelectorPerLine,
         autoprefixer()
     ]
 };
